69e6f83914a148acdb8569edf2b92598d227a4a3
GUNAVIJAY01 <gunanathan0@gmail.com>
2023-08-31 10:51:07 +0530

    update

--- rio.js
+++ rio.js
@@ -1,43 +1,54 @@
+		window.addEventListener("scroll",change)
 		
 		function change(){
 		var a=document.getElementById("rex")
 		var b=document.getElementById("about")
 		var c=document.getElementById("resume")
 		var d=document.getElementById("portfolio")
-		var e=document.getElementById("skill")
+		var e=document.getElementById("gu")
 		var scrollvalue=window.scrollY
 		console.log(scrollvalue)	
-		if(scrollvalue<988){
+		if(scrollvalue<900){
 			 b.setAttribute("class","p d-block");
 			 c.setAttribute("class","p d-none");
 			a.setAttribute("class","rex-1 ")
 		}
 
 		
-		else if(scrollvalue>988 && scrollvalue<1200){
+		else if(scrollvalue<1350){
 				 d.setAttribute("class","p d-none");
 				 b.setAttribute("class","p d-none");
 			c.setAttribute("class","p d-block");
 					a.setAttribute("class","rex-2")
 				}
 			
-		else if	(scrollvalue>1400){
+		else if	(scrollvalue<1700){
 				 d.setAttribute("class","p d-block");
 				 c.setAttribute("class","p d-none");
+				 e.setAttribute("class","p d-none");
+
 			
 			a.setAttribute("class","rex-3")
 			}
-		else if	(scrollvalue>1200){
+		else if	(scrollvalue>1600){
+				 d.setAttribute("class","p d-none");
+				
+				e.setAttribute("class","p d-block")
+				console.log(scrollvalue)
+			a.setAttribute("class","rex-0")
+
 		}
 	
 		}
-		window.addEventListener("scroll",change)
 			
 		// home-sectiom
+			let homes=document.getElementById("nav-home")	
+
+
 
 		function home(){
-			console.log("guna")
-			homes.classList.toggle("d-none")
+			 console.log("guna")
+			homes.classList.toggle("nahome")
+		
 		}
-			let homes=document.getElementById("nav-home")	
 			
